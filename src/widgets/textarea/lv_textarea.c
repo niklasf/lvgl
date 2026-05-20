a44f31a0019c625a153db660c547c6e1d8969a97
Niklas Fiekas <niklas.fiekas@backscattering.de>
2026-05-20 21:38:22 +0200

    same for size changed

--- src/widgets/textarea/lv_textarea.c
+++ src/widgets/textarea/lv_textarea.c
@@ -927,11 +927,7 @@ static void lv_textarea_event(const lv_obj_class_t * class_p, lv_event_t * e)
     else if(code == LV_EVENT_DRAW_POST) {
         draw_cursor(e);
     }
-    else if(code == LV_EVENT_SIZE_CHANGED) {
-        lv_textarea_t * ta = (lv_textarea_t *)obj;
-        lv_textarea_scroll_to_cusor_pos(obj, ta->cursor.pos);
-    }
-    else if(code == LV_EVENT_STYLE_CHANGED) {
+    else if(code == LV_EVENT_SIZE_CHANGED || code == LV_EVENT_STYLE_CHANGED) {
         lv_obj_readjust_scroll(obj, LV_ANIM_ON);
     }
 }
